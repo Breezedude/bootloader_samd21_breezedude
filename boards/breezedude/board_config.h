1f045e1e8806fd34aa2743dd9a0acb292b341003
Florian Schmid <schmid.flori@t-online.de>
2026-04-14 14:52:27 +0200

    init_bd

---
+++ boards/breezedude/board_config.h
@@ -0,0 +1,20 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Breezedude"
+#define PRODUCT_NAME "Breezedude"
+#define VOLUME_LABEL "BreezeBOOT"
+#define INDEX_URL "https://breezedude.de/configurator.html"
+#define BOARD_ID "SAMD21G18A-Breezedude"
+
+#define USB_VID 0x6666
+#define USB_PID 0xBD00
+
+#define LED_PIN PIN_PA17
+
+//#define BOARD_RGBLED_CLOCK_PIN            PIN_PA00
+//#define BOARD_RGBLED_DATA_PIN             PIN_PA01
+
+#endif
